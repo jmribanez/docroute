40296bf7ad4daebe1db9afc0ae6899da06e70dcd
jmribanez <49083916+jmribanez@users.noreply.github.com>
2024-04-06 22:10:25 +0800

    Prepared documentroute controller
    Placed comments to guide incoming implementation.

--- app/Http/Controllers/DocumentRouteController.php
+++ app/Http/Controllers/DocumentRouteController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class DocumentRouteController extends Controller
 {
-    //
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function receive(string $id) {
+        // This function receives the document_id
+        // It will call a collection of records with this document_id
+        // It will append the present date and time, user, and their present office to the new record.
+    }
 }
